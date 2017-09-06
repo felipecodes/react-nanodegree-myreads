c066c6fb4b7a59f56d8dd626a36dcd5c2bcb74d8
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-09-06 09:27:27 -0300

    add book component and its tests

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-function Book(props) {
-  const {
-    book,
-    addTocurrentlyReading,
-    addToWantRead,
-    addToRead
-  } = props
+class Book extends Component {
+  state = { list: '' }
 
-  return (
-    <li>
-      {book.title}
-    </li>
-  )
+  componentWillUpdate() {
+    switch (this.state.list) {
+      case 'Currently Reading':
+        this.props.addTocurrentlyReading(this.props.book)
+        break
+      case 'Want to Read':
+        this.props.addToWantRead(this.props.book)
+        break
+      case 'Read':
+        this.props.addToRead(this.props.book)
+        break
+      default:
+    }
+  }
+
+  handleChange = event => {
+    event.preventDefault()
+    this.setState({ list: event.target.value })
+  }
+
+  render() {
+    const { book } = this.props
+    return (
+      <li>
+        <h1>{book.title}</h1>
+        <h2>{book.subtitle}></h2>
+
+        <select onChange={this.handleChange}>
+          <option></option>
+          <option>Currently Reading</option>
+          <option>Want Read</option>
+          <option>Read</option>
+        </select>
+      </li>
+    )
+  }
 }
 
 Book.propTypes = {
