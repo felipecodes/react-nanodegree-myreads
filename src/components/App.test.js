5d97b9f862532527661d78d0ed48a81c99e5266c
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-09-14 09:11:23 -0300

    add test of addToCurrentlyReading method
     - The test expects that the book added to the list is already in the
    list

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -41,38 +41,66 @@ describe('App component', () => {
 
   it('data fetch in componentDidMount', () => {
     promise.then(() => {
-      expect(wrapper.state('books').byId).toHaveProperty('jAUODAAAQBAJ')
-      expect(wrapper.state('books').allIds.indexOf('jAUODAAAQBAJ') > -1).toEqual(true)
+      const BOOK = 'jAUODAAAQBAJ'
+      const { byId, allIds} = wrapper.state('books')
+      expect(byId).toHaveProperty(BOOK)
+      expect(allIds.indexOf(BOOK) > -1).toEqual(true)
     })
   })
 
-  it('adding book in currently reading list', () => {
-    wrapper.node.addTocurrentlyReading({ id: 'jAUODAAAQBAJ' })
-    expect(wrapper.state('books').currentlyReadingIds.indexOf('jAUODAAAQBAJ') > -1)
+  it('adding book in currently list', () => {
+    const BOOK = 'jAUODAAAQBAJ'
+    wrapper.node.addTocurrentlyReading({ id: BOOK })
+    expect(wrapper.state('books').currentlyReadingIds.indexOf(BOOK) > -1)
       .toEqual(true)
-    expect(wrapper.state('books').wantReadIds.indexOf('jAUODAAAQBAJ') > -1)
+    expect(wrapper.state('books').wantReadIds.indexOf(BOOK) > -1)
       .toEqual(false)
-    expect(wrapper.state('books').readIds.indexOf('jAUODAAAQBAJ') > -1)
+    expect(wrapper.state('books').readIds.indexOf(BOOK) > -1)
       .toEqual(false)
   })
 
-  it('adding book in want to read list', () => {
-    wrapper.node.addToWantRead({ id: 'jAUODAAAQBAJ' })
-    expect(wrapper.state('books').wantReadIds.indexOf('jAUODAAAQBAJ') > -1)
+  it('adding book in want list', () => {
+    const BOOK = 'sJf1vQAACAAJ'
+    wrapper.node.addToWantRead({ id: BOOK })
+    expect(wrapper.state('books').wantReadIds.indexOf(BOOK) > -1)
       .toEqual(true)
-    expect(wrapper.state('books').currentlyReadingIds.indexOf('jAUODAAAQBAJ') > -1)
+    expect(wrapper.state('books').currentlyReadingIds.indexOf(BOOK) > -1)
       .toEqual(false)
-    expect(wrapper.state('books').readIds.indexOf('jAUODAAAQBAJ') > -1)
+    expect(wrapper.state('books').readIds.indexOf(BOOK) > -1)
       .toEqual(false)
   })
 
   it('adding book in read list', () => {
-    wrapper.node.addToRead({ id: 'jAUODAAAQBAJ' })
-    expect(wrapper.state('books').readIds.indexOf('jAUODAAAQBAJ') > -1)
+    const BOOK = 'nggnmAEACAAJ'
+    wrapper.node.addToRead({ id: BOOK })
+    expect(wrapper.state('books').readIds.indexOf(BOOK) > -1)
       .toEqual(true)
-    expect(wrapper.state('books').currentlyReadingIds.indexOf('jAUODAAAQBAJ') > -1)
+    expect(wrapper.state('books').currentlyReadingIds.indexOf(BOOK) > -1)
       .toEqual(false)
-    expect(wrapper.state('books').wantReadIds.indexOf('jAUODAAAQBAJ') > -1)
+    expect(wrapper.state('books').wantReadIds.indexOf(BOOK) > -1)
+      .toEqual(false)
+  })
+
+  it('adding the book from currently list to currently list', () => {
+    const BOOK = 'jAUODAAAQBAJ'
+
+    wrapper.node.addTocurrentlyReading({ id: BOOK })
+
+    const {
+      currentlyReadingIds,
+      wantReadIds,
+      readIds
+    } = wrapper.state('books')
+    const isBookInWantList = wantReadIds.indexOf(BOOK) > -1
+    const isBookInReadList = readIds.indexOf(BOOK) > -1
+    const booksInCurrently = currentlyReadingIds.filter(id => id === BOOK)
+    const [book] = booksInCurrently
+
+    expect(booksInCurrently.length === 1 && book === BOOK)
+      .toEqual(true)
+    expect(isBookInWantList)
+      .toEqual(false)
+    expect(isBookInReadList)
       .toEqual(false)
   })
 
