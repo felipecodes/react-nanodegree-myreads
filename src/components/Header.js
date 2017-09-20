2b1ec65f4f3f5245bbbb8bf04d44d8cf43496886
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-09-20 07:32:15 -0300

    add header icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import ActionHome from 'material-ui/svg-icons/action/home'
 
 function Header() {
   return (
     <AppBar
       title="My Reads"
-      iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+      iconElementLeft={<IconButton><Link to="/"><ActionHome style={{color: '#fff'}} /></Link></IconButton>}
+      iconClassNameRight="muidocs-icon-navigation-expand-more" />
   )
 }
 
