30d0ddf3de939c269e2bd20a5a2925fad091a300
Felipe Oliveira <felipe.o.silva18@gmail.com>
2017-09-25 17:49:13 -0300

    move from stateful to functional component

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,37 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import SearchBar from './SearchBar'
 import BookList from './BookList'
 import css from './SearchPage.css'
 
-class SearchPage extends Component {
-  render() {
-    const {
-      byId,
-      allIds,
-      addTocurrentlyReading,
-      addTowantToRead,
-      addToRead,
-      searchBooks,
-      searchClean,
-      getList,
-      removeShelf
-    } = this.props
+function SearchPage(props) {
+  const {
+    byId,
+    allIds,
+    addTocurrentlyReading,
+    addTowantToRead,
+    addToRead,
+    searchBooks,
+    removeShelf
+  } = props
 
-    const books = allIds.map(id => byId[id])
+  const books = allIds.map(id => byId[id])
 
-    return (
-      <div id="search-page" className={css.searchPage}>
-        <SearchBar searchBooks={searchBooks} searchClean={searchClean} />
-        <BookList
-          books={books}
-          addTocurrentlyReading={addTocurrentlyReading}
-          addTowantToRead={addTowantToRead}
-          addToRead={addToRead}
-          removeShelf={removeShelf}
-          getList={getList} />
-      </div>
-    )
-  }
+  return (
+    <div id="search-page" className={css.searchPage}>
+      <SearchBar searchBooks={searchBooks} />
+      <BookList
+        books={books}
+        addTocurrentlyReading={addTocurrentlyReading}
+        addTowantToRead={addTowantToRead}
+        addToRead={addToRead}
+        removeShelf={removeShelf} />
+    </div>
+  )
 }
 
 export default SearchPage
